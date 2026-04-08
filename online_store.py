1de9a44a4dae5cb9b263a1873a9ca48de3c6bb5d
wslider <wslider2000@gmail.com>
2026-04-08 17:30:34 -0400

    initial structure of online store with product class, list of product objects and functions

--- online_store.py
+++ online_store.py
@@ -6,7 +6,7 @@ class Product:
 
 
 
-Store = [
+store = [
     Product("Slip and Slide", 10, 3), 
     Product("Artisan Bread", 3, 10), 
     Product("GMO Pet Hamster", 5, 5), 
@@ -17,20 +17,107 @@ Store = [
 
 class ShoppingCart:
     def __init__(self):
-        self.items = {}
-        self.total = 0.0
+        self.items = {}  # {product: quantity}
 
-    def add_product():
+    def display_available_products(self, store):
+        print("\nAvaliable Products in Our Store: ")
+        # enumerate objects in store list .. starting at 1
+        print("💸" * 30) 
+        for i, product in enumerate(store, 1):
+            print(f"{i}: {product.name}  Price: ${product.price}  Inventory: {product.stock}")
+            print("-" * 30)
+        print("=" * 30)
+
+
+    def add_product(self, store): 
+        
+        self.display_available_products(store)
+
+        try: 
+            choice = int(input("Please enter your product number: "))
+            if choice < 1 or choice > len(store):
+                print("Please enter a valid number from our selection of inventory!")
+            
+            selected_product = store[choice - 1]
+
+            quantity = int(input(f"How many {selected_product}s would you like?"))
+            if quantity < 1:
+                print("Quantity must be more than 0")
+                return
+            if quantity > selected_product.stock:
+                print("Not enough {selected_product}s in stock! Please choose a lesser ammount.")
+                return
+            
+            
+
+        except ValueError:
+            print("Enter a valid product number!")
+
+
+
+            # check if available  
+                
+
+        # remove from stock
+
+
+        # add to cart 
+
+            # check if already in cart 
+
+
+    def remove_product(self, product, quantity):
+
+        # show items in cart 
+
+        # select product
+        
+        # remove from cart
+
+        # add to stock
+
+    def view_cart(self):
         pass
 
-    def remove_product():
+    def get_total(self):
         pass
-
-    def check_cart(self):
-        print(self.items)
 
     def checkout(self):
-        pass 
+       pass
+
+
+# while loop - while not checked out 
+while ShoppingCart.checkout(): 
+    # display options
+    print("What would you like to do?")
+    print("1. Add Product to Cart")
+    print("2. Remove Product from Cart")
+    print("3. View Cart")
+    print("4. View Total")
+    print("5. Checkout") 
+
+
+    # user input: select action ( enter option number)
+
+    choice = input("Enter your choice: ( 1 - 5 )")
+
+    if choice == 1: 
+        ShoppingCart.add_product()
+    elif choice == 2:
+        ShoppingCart.remove_product()
+    elif choice == 3: 
+        ShoppingCart.view_cart()
+    elif choice == 4:
+        ShoppingCart.get_total()
+    elif choice == 5:
+        ShoppingCart.checkout() 
+    else:
+        print("Please enter a valid choice: ")
+
+
+
+
+    
 
     
 
